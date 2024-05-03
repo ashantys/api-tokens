99b16859121302cb261c38e05595d767aa9d34ac
Ashanty Sierra <ashanty.sierram@gmail.com>
2024-05-03 10:52:27 -0600

    feat(se crearon los controladores) se crearon las funciones de los controladores

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,8 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Character;
 
 class CharacterController extends Controller
 {
-    //
+    // Get all the characters
+    public function index()
+    {
+        return Character::with('series')->get();
+    }
+
+    // Create a character
+    public function store(Request $request)
+    {
+        $character = Character::create($request->all());
+        $character->series()->attach($request->input('serie_id'));
+        return response()->json($character, 201);
+    }
+
+    // Get a character
+    public function show($id)
+    {
+        return Character::with('series')->find($id);
+    }
+
+    // Update character
+    public function update(Request $request, $id)
+    {
+        $character = Character::findOrFail($id);
+        $character->update($request->all());
+        $character->series()->sync($request->input('serie_id'));
+        return response()->json($character, 200);
+    }
+
+    // Delete character
+    public function destroy($id)
+    {
+        $character = Character::findOrFail($id);
+        $character->delete();
+        return response()->json(null, 204);
+    }
+
 }
--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Serie;
 
 class SerieController extends Controller
 {
-    //
+        // Get series
+        public function index()
+        {
+            return Serie::all();
+        }
+
+        //Get a new series
+        public function store(Request $request)
+        {
+            $serie = Serie::create($request->all());
+            return response()->json($serie, 201);
+        }
+
+        // Get specific serie
+        public function show($id)
+        {
+            return Serie::find($id);
+        }
+
+        // Update a serie
+        public function update(Request $request, $id)
+        {
+            $serie = Serie::findOrFail($id);
+            $serie->update($request->all());
+            return response()->json($serie, 200);
+        }
+
+        // Delete a serie
+        public function destroy($id)
+        {
+            $serie = Serie::findOrFail($id);
+            $serie->delete();
+            return response()->json(null, 204);
+        }
 }
